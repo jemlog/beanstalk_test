b6628ac6083f258d6586603249df478d23fc716f
jemin <jemin03120111@gmail.com>
2022-10-02 00:47:28 +0900

    test: beanstalk 기본 설정

---
+++ src/test/java/com/example/beanstalk/BeanstalkApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.beanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class BeanstalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
